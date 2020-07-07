78e1ff685b0f038823bb096ebf6a58d89f4268b3
bonliu <cheukbong1210@gmail.com>
2020-07-07 15:43:16 -0400

    Add home page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,26 +6,21 @@ import {
 } from 'react-router-dom';
 import '../styles/App.css';
 
+import Home from './Home';
 import Login from './Login';
 import Register from './Register';
 import Portfolio from './Portfolio';
 
 function App() {
   return (
-    <div>
-      <h1>Stock Pro</h1>
-      <a href="/login">Login</a>
-      <br></br>
-      <a href="/signup">Signup</a>
-      <Router>
-        <Switch>
-          <Route path="/login" exact component={Login}></Route>
-          <Route path="/signup" component={Register}></Route>
-          <Route path="/portfolio" component={Portfolio}></Route>
-        </Switch>
-      </Router>
-    </div>
-    
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home}></Route>
+        <Route path="/login" component={Login}></Route>
+        <Route path="/signup" component={Register}></Route>
+        <Route path="/portfolio" component={Portfolio}></Route>
+      </Switch>
+    </Router>
   );
 }
 
